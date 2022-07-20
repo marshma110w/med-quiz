9a3060e62942d23b4700385fb173f9550f35940e
Michael <m.chichunov@erlyvideo.org>
2022-07-20 11:24:58 +0300

    added-models-validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,11 +3,24 @@
 # Table name: users
 #
 #  id         :bigint           not null, primary key
-#  email      :string
-#  name       :string
+#  email      :string           not null
+#  name       :string           not null
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
 #
 class User < ApplicationRecord
-    has_many :attempts, dependent: :destroy
+  has_many :attempts, dependent: :destroy
+
+  validates :email, uniqueness: { case_sensetive: false }
+  validates :name, presence: true
+  validates :email, presence: true
+
+  before_validation :downcase_email
+
+  private
+
+  def downcase_email
+    email.downcase!        
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -30,8 +30,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_20_073924) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.string "email"
+    t.string "name", null: false
+    t.string "email", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
