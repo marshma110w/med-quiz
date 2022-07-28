93508c62cb0d249e4a3f501695e8406a6e7333db
Michael <m.chichunov@erlyvideo.org>
2022-07-28 19:53:15 +0300

    deleted-fantom-table

--- db/schema.rb
+++ db/schema.rb
@@ -22,13 +22,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_27_054339) do
     t.index ["user_id"], name: "index_attempts_on_user_id"
   end
 
-  create_table "tutorials", force: :cascade do |t|
-    t.string "title"
-    t.string "url"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "name", null: false
     t.string "email", null: false
