85c80e905205c1aefd2cf7e17c7b34093f154b03
Michael <m.chichunov@erlyvideo.org>
2022-07-29 18:28:07 +0300

    catching-user-answer

--- app/controllers/quiz_controller.rb
+++ app/controllers/quiz_controller.rb
@@ -1,4 +1,11 @@
 class QuizController < ApplicationController
+
+  skip_before_action :verify_authenticity_token, only: :user_answer
+
   def try
+  end
+
+  def user_answer
+    render plain: params
   end
 end
