73890d78442b9c0b925df0d513adccdd525608a5
Michael <m.chichunov@erlyvideo.org>
2022-08-14 21:06:40 +0300

    fixed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
-      sign_in @user
+      log_in @user
       flash[:success] = "Welcome back, #{@user.name}!"
       redirect_to quiz_try_url
     else
